dcaf590e92cbe45a3567674aad78b61b7d16e3aa
Luís Macedo <36333166+FallenFoil@users.noreply.github.com>
2025-01-18 19:52:02 +0000

    Update Typer.py

--- Typer.py
+++ Typer.py
@@ -4,12 +4,12 @@ import keyboard
 
 def typer(text):
   for char in text:
-    if char == '!':
-      keyboard.press_and_release('shift+1')
-    elif char == '@':
-      keyboard.press_and_release('shift+2')
+    if char in ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '{', '}', ':', '"', '|', '<', '>', '?']:
+      keyboard.press_and_release(keyboard.get_hotkey_name(['left shift', char]))
+    elif char in ['€']:
+      keyboard.press_and_release(keyboard.get_hotkey_name(['left ctrl', 'left alt', char]))
     elif char in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']:
-      keyboard.press_and_release(keyboard.get_hotkey_name(['left shift'], char.lower()))
+      keyboard.press_and_release(keyboard.get_hotkey_name(['left shift', char.lower()]))
     else:
       keyboard.press_and_release(char)
 
